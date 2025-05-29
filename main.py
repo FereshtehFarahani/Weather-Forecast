e1e6071ad807cc98565dba3527ae2342812f2e58
Fereshteh Farahani <f.feizabadifarahani@gmail.com>
2025-05-29 11:20:29 +0200

    input for city

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 API_KEY = ""
-CITY = "Turin"
+CITY = input("Enter city name: ")
 res = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}"
 response = requests.get(res)
 if response.status_code == 200:
@@ -10,6 +10,7 @@ if response.status_code == 200:
 else:
     print("Error:", response.status_code)
 
+# extracting needed data 
 city = data['name']
 weather_desc = data['weather'][0]['description']
 temp = data['main']['temp']
@@ -19,3 +20,6 @@ print(f"City: {city}")
 print(f"Temperature: {temp}°C")
 print(f"Weather: {weather_desc}")
 print(f"Humidity: {humidity}%")
+
+
+
