441cd13e3bb2c6ba4854f0f765cf2ffb50ae890b
Fereshteh Farahani <f.feizabadifarahani@gmail.com>
2025-05-29 11:13:23 +0200

    Weather data retrieved

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Weather-Forecast
+🛠️ Phase 1: CLI Version – Core Functionality
+✅ Step 1: Setup
+Install required libraries: requests, json
---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+API_KEY = ""
+CITY = "Turin"
+res = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}"
+response = requests.get(res)
+if response.status_code == 200:
+    data = response.json()  # parse JSON response
+    #print(data)
+else:
+    print("Error:", response.status_code)
+
+city = data['name']
+weather_desc = data['weather'][0]['description']
+temp = data['main']['temp']
+humidity = data['main']['humidity']
+
+print(f"City: {city}")
+print(f"Temperature: {temp}°C")
+print(f"Weather: {weather_desc}")
+print(f"Humidity: {humidity}%")
