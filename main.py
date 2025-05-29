a08296f596b94dedbac0e5ced2ca03fedd695d4b
Fereshteh Farahani <f.feizabadifarahani@gmail.com>
2025-05-29 14:23:24 +0200

    error logging and handeling

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 import requests
 
 API_KEY = ""
-CITY = input("Enter city name: ")
-res = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}"
-response = requests.get(res)
-if response.status_code == 200:
-    data = response.json()  # parse JSON response
-    #print(data)
-else:
-    print("Error:", response.status_code)
+while True:
+    CITY = input("Enter city name: ")
+    res = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}"
+    try:
+        response = requests.get(res)
+        if response.status_code == 200:
+            data = response.json()  # parse JSON response
+            #print(data)
+        else:
+            print("Error:", response.status_code)
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
 
-# extracting needed data 
-city = data['name']
-weather_desc = data['weather'][0]['description']
-temp = data['main']['temp']
-humidity = data['main']['humidity']
+    if 'data' not in locals():  
+        print("Failed to retrieve data. Please check the city name and try again.")
+        continue
+    
+    # extracting needed data 
+    city = data['name']
+    weather_desc = data['weather'][0]['description']
+    temp = data['main']['temp']
+    humidity = data['main']['humidity']
+    
+    print(f"City: {city}")
+    print(f"Temperature: {temp}°C")
+    print(f"Weather: {weather_desc}")
+    print(f"Humidity: {humidity}%")
 
-print(f"City: {city}")
-print(f"Temperature: {temp}°C")
-print(f"Weather: {weather_desc}")
-print(f"Humidity: {humidity}%")
+    if input("Do you want to continue? (yes/no): ").lower() == 'yes':
+        continue
+    else:
+        print("Exiting the program.")
+        break
+
+
 
 
 
